dcb28448a39ac89b3c29d9e1d0ce61c960c12002
Humberto <humberto.c.lima18@gmail.com>
2022-05-31 20:40:28 -0300

    Implementa Servidor HTTP

---
+++ Server-Http/serverHTTP.py
@@ -0,0 +1,30 @@
+from http.server import BaseHTTPRequestHandler, HTTPServer
+from datetime import datetime
+import time
+
+hostIP = "127.0.0.1"
+porta = 5000
+
+class Servidor(BaseHTTPRequestHandler):
+    def do_GET(self):
+        now = datetime.now()
+        hora_atual = now.strftime("%H:%M:%S")
+        self.send_response(200)
+        self.send_header("Content-type", "text/html")
+        self.end_headers()
+        self.wfile.write(bytes("<html><head><title>Servidor HTTP</title></head>", "utf-8"))
+        self.wfile.write(bytes("<body>", "utf-8"))
+        self.wfile.write(bytes("<p>Agora sao: %s</p>" % (hora_atual), "utf-8"))
+        self.wfile.write(bytes("</body></html>", "utf-8"))
+
+if __name__ == "__main__":        
+    servidorWeb = HTTPServer((hostIP, serverPort), Servidor)
+    print("Servidor iniciado em %s:%s" % (hostIP, serverPort))
+
+    try:
+        servidorWeb.serve_forever()
+    except KeyboardInterrupt:
+        pass
+
+    servidorWeb.server_close()
+    print("Servidor interrompido.")
